fa88a2a3d493b9deb7e0bb4deae9beb703ab4b6e
Mariano Heredia <mariano@kartjuba.com>
2016-01-20 18:16:46 -0300

    Added armv7 & x86_64 architectures. Fixed an issue when creating libgsl_all.a the find command was filtering lib.*a files recursively and was taking .a files from outdir directory which lead to mixed architectures.

--- build_ios.sh
+++ build_ios.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 LOCAL_OUTDIR="./outdir"
-IOS_DEPLOY_TGT="8.0"
+IOS_DEPLOY_TGT="7.0"
 
 setenv_all()
 {
@@ -25,6 +25,18 @@ setenv_arm7s()
     export TCROOT=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin
 
 	export CFLAGS="-arch armv7s -pipe -no-cpp-precomp -isysroot $SDKROOT -miphoneos-version-min=$IOS_DEPLOY_TGT -I$SDKROOT/usr/include/"
+	
+	setenv_all
+}
+
+setenv_arm7()
+{
+    unset SDKROOT TCROOT CFLAGS CC LD CXX AR AS NM RANLIB LDFLAGS CXXFLAGS CPPFLAGS
+
+    export SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
+    export TCROOT=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin
+
+	export CFLAGS="-arch armv7 -pipe -no-cpp-precomp -isysroot $SDKROOT -miphoneos-version-min=$IOS_DEPLOY_TGT -I$SDKROOT/usr/include/"
 	
 	setenv_all
 }
@@ -53,13 +65,29 @@ setenv_i386()
 	setenv_all
 }
 
+setenv_x86_64()
+{
+	unset SDKROOT TCROOT CFLAGS CC LD CXX AR AS NM RANLIB LDFLAGS CXXFLAGS CPPFLAGS
+
+    export SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk
+    export TCROOT=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin
+
+	export CFLAGS="-arch x86_64 -pipe -no-cpp-precomp -isysroot $SDKROOT -miphoneos-version-min=$IOS_DEPLOY_TGT"
+	
+	setenv_all
+}
+
+
 create_outdir_lipo()
 {
 	for lib_i386 in `find $LOCAL_OUTDIR/i386 -name "lib*.a"`; do
         lib_arm7s=`echo $lib_i386 | sed "s/i386/arm7s/g"`
+        lib_arm7=`echo $lib_i386 | sed "s/i386/arm7/g"`
         lib_arm64=`echo $lib_i386 | sed "s/i386/arm64/g"`
+        lib_x86_64=`echo $lib_i386 | sed "s/i386/x86_64/g"`
 		lib=`echo $lib_i386 | sed "s/i386//g"`
-		xcrun -sdk iphoneos lipo -arch arm64 $lib_arm64 -arch armv7s $lib_arm7s -arch i386 $lib_i386 -create -output $lib
+		xcrun -sdk iphoneos lipo -arch x86_64 $lib_x86_64 -arch arm64 $lib_arm64 -arch armv7s $lib_arm7s -arch armv7 $lib_arm7 -arch i386 $lib_i386 -create -output $lib
+
 	done
 }
 
@@ -78,14 +106,31 @@ merge_libfiles()
 }
 
 rm -rf $LOCAL_OUTDIR
-mkdir -p $LOCAL_OUTDIR/i386 $LOCAL_OUTDIR/arm7s $LOCAL_OUTDIR/arm64
+mkdir -p $LOCAL_OUTDIR/i386 $LOCAL_OUTDIR/arm7 $LOCAL_OUTDIR/arm7s $LOCAL_OUTDIR/arm64 $LOCAL_OUTDIR/x86_64
+
+make clean 2> /dev/null
+make distclean 2> /dev/null
+setenv_x86_64
+./configure --enable-shared=no -enable-static
+make
+for i in `find . -path $LOCAL_OUTDIR -prune -o -name "lib*.a" -print | grep -v arm`; do cp -rvf $i $LOCAL_OUTDIR/x86_64; done
+merge_libfiles $LOCAL_OUTDIR/x86_64 libgsl_all.a
+
+make clean 2> /dev/null
+make distclean 2> /dev/null
+setenv_arm7
+./configure --host=arm-apple-darwin7s --enable-shared=no -enable-static
+make
+for i in `find . -path $LOCAL_OUTDIR -prune -o -name "lib*.a" -print | grep -v arm`; do cp -rvf $i $LOCAL_OUTDIR/arm7; done
+merge_libfiles $LOCAL_OUTDIR/arm7 libgsl_all.a
+
 
 make clean 2> /dev/null
 make distclean 2> /dev/null
 setenv_arm7s
 ./configure --host=arm-apple-darwin7s --enable-shared=no -enable-static
 make
-for i in `find . -name "lib*.a" | grep -v arm`; do cp -rvf $i $LOCAL_OUTDIR/arm7s; done
+for i in `find . -path $LOCAL_OUTDIR -prune -o -name "lib*.a" -print | grep -v arm`; do cp -rvf $i $LOCAL_OUTDIR/arm7s; done
 merge_libfiles $LOCAL_OUTDIR/arm7s libgsl_all.a
 
 make clean 2> /dev/null
@@ -93,7 +138,7 @@ make distclean 2> /dev/null
 setenv_arm64
 ./configure --host=arm-apple-darwin64 --enable-shared=no -enable-static
 make
-for i in `find . -name "lib*.a" | grep -v arm`; do cp -rvf $i $LOCAL_OUTDIR/arm64; done
+for i in `find . -path $LOCAL_OUTDIR -prune -o -name "lib*.a" -print | grep -v arm`; do cp -rvf $i $LOCAL_OUTDIR/arm64; done
 merge_libfiles $LOCAL_OUTDIR/arm64 libgsl_all.a
 
 make clean 2> /dev/null
@@ -101,7 +146,7 @@ make distclean 2> /dev/null
 setenv_i386
 ./configure --enable-shared=no -enable-static
 make
-for i in `find . -name "lib*.a" | grep -v arm`; do cp -rvf $i $LOCAL_OUTDIR/i386; done
+for i in `find . -path $LOCAL_OUTDIR -prune -o -name "lib*.a" -print | grep -v arm`; do cp -rvf $i $LOCAL_OUTDIR/i386; done
 merge_libfiles $LOCAL_OUTDIR/i386 libgsl_all.a
 
 create_outdir_lipo
